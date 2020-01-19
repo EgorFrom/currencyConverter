1c33042f764a4318f268a8a51a9441087f501d31
nagibin <e.s.nagibin@tmn3.etagi.com>
2020-01-19 10:13:39 +0500

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by mac-223 on 19/01/2020.
+//  Copyright © 2020 Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
